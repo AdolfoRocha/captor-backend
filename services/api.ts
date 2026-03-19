a78332f37e949c98ae68b84125a110bd35b5b3f4
Adolfo Rocha de Araujo <adolforocha@MacBook-Pro-de-Adolfo.local>
2026-03-19 17:00:07 -0300

    feat: use final serveo tunnel link

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import { firestoreMissionsApi, firestoreTargetsApi, firestoreAgentApi, firestore
 
 export const isProduction = import.meta.env.MODE === 'production';
 
-export const API_BASE = 'https://illustrated-labor-tribes-elephant.trycloudflare.com/api';
+export const API_BASE = 'https://a3d328327a17f4fa-35-239-48-106.serveousercontent.com/api';
 
 // Helper for Fetch API
 const api = {
